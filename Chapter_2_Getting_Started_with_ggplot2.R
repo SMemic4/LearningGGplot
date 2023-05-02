d61b2a3bf67984e909342fb910b7778ae0ae8515
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-05-02 13:41:26 -0400

    Update Chapter_2_Getting_Started_with_ggplot2.R

--- Chapter_2_Getting_Started_with_ggplot2.R
+++ Chapter_2_Getting_Started_with_ggplot2.R
@@ -288,13 +288,13 @@ ggplot(mpg, aes(displ, hwy)) + geom_point() + geom_smooth(method = "lm")
 ###############################################################################################################################################################################################
 # 2.6.2 Boxplots and Jittered Points
 ###############################################################################################################################################################################################
-# When a set of data contains a cateogorical variable and one ore more continuous variables, it is most likely there will be interest in knowing how the values of the continuous variables vary with the levels of the categorical variables
+# When a set of data contains a categorical variable and one ore more continuous variables, it is most likely there will be interest in knowing how the values of the continuous variables vary with the levels of the categorical variables
 
 ggplot(mpg, aes(drv, hwy)) + geom_point()
 
-# The data above has few unique values of both class and hwy leading to a lot of overlap on the plot, due to many points being plotted in the same location, making it harder to udnerstand the distrubtion
+# The data above has few unique values of both class and hwy leading to a lot of overlap on the plot, due to many points being plotted in the same location, making it harder to understand the distribution
 # There are three useful techniques to help alleviate the problem:
-# 1. Jittering, geom_jitter(), adds a little random noise to the data which cna help avoid overplotting
+# 1. Jittering, geom_jitter(), adds a little random noise to the data which can help avoid overplotting
 
 ggplot(mpg, aes(drv, hwy)) + geom_jitter()
 
@@ -309,10 +309,10 @@ ggplot(mpg, aes(drv, hwy)) + geom_violin()
 # Each of the methods has its strengths and weaknesses. 
 # Boxplot summarise the bulk of the distribution with only five numbers
 # Jittered plots show every point but only work with relatively small datasets
-# Violoin plots give the richest display, but rely on the calculation of a density estimate, which can be hard to interpret
+# Violin plots give the richest display, but rely on the calculation of a density estimate, which can be hard to interpret
 
 # For jittered points, geom_jitter() offers the same control over aesthetics as geom_point(): size, color, and shape
-# For geom_boxplot() and geom_violin() the outline color and internetal color fill can be chosen
+# For geom_boxplot() and geom_violin() the outline color and internal color fill can be chosen
 
 ###############################################################################################################################################################################################
 # 2.6.3 Histograms and Frequency Polygons
@@ -320,13 +320,216 @@ ggplot(mpg, aes(drv, hwy)) + geom_violin()
 # Histograms and Frequency Polygons show the distribution of a single numeric variable. They provide more information about the distribution of a single group than boxplots, at the expense of needing more space
 
 ggplot(mpg, aes(hwy)) + geom_histogram()
-
 ggplot(mpg, aes(hwy)) + geom_freqpoly()
 
-# Both histograms and frequency polygons work in the same way: they bin the data, and count the number of observations in each bin. The only difference between the two is the display. Histrograms use bars while frequency plots use lines
+# Both histograms and frequency polygons work in the same way: they bin the data, and count the number of observations in each bin. The only difference between the two is the display. Histograms use bars while frequency plots use lines
 # The width of the bins can be controlled with the binwidth argument (to avoid evenly spaced bins use the breaks argument)
 # It is important to experiment with the bin width. The default just splits the data into 30 bins, which is unlikely to be the best option of representations
 
-ggplot()
+ggplot(mpg, aes(hwy)) + geom_freqpoly(binwidth = 2.5)
+ggplot(mpg, aes(hwy)) + geom_freqpoly(binwidth = 1)
 
+# An alternative to the frequency polygon is the density plot, geom_density(). These plots have a drawback of being harder to interpret since the underlying computations are much more complex. Additionally they also make assumptions that are not true for all data, namely that the underlying distribution is continuous, unbounded, and smooth
 
+ggplot(mpg,aes(hwy)) + geom_density()
+
+# To compare the distributions of different subgroups, a categorical variable can be mapped to either fill (for geom_histogram()) or color (for geom_freqpoly())
+# It's easier to compare distributions using the frequency polygon because the underlying perceptual task is easier.
+# Additionally faceting can also be used. This makes comparisons a little harder, but it's easier to see the distribution of each group
+
+ggplot(mpg, aes(displ, color = drv)) + geom_freqpoly(binwidth = 0.5)
+ggplot(mpg, aes(displ, fill = drv)) + geom_histogram(binwidth = 0.5) + facet_wrap(~drv, ncol = 1)
+
+###############################################################################################################################################################################################
+# 2.6.4 Bar Charts
+###############################################################################################################################################################################################
+# The discrete analogue of he histogram is the bar chart geom_bar(). It is easy to use
+
+ggplot(mpg, aes(manufacturer)) + geom_bar()
+
+# Bar charts are unique in R
+# The bar cart above expects to have summarized data, and each observation contributes one unit to the height of each bar. The other for of bar chart is used to presummarised data
+
+drugs <- data.frame(drug = c("a", "b", "c"), effect = c(4.2, 9.7, 6.1))
+
+# To display this sort of data, geom_bar() needs to be told not to run the default stat which bins and counts the data
+
+ggplot(drugs, aes(drug, effect)) + geom_bar(stat = "identity")
+
+###############################################################################################################################################################################################
+# 2.6.5. Time Series with Line and Path Plots 
+###############################################################################################################################################################################################
+# Line and path plots are typically used for time series data. Line plots join the points from left to right, while path plots join them in the order that they appear in the dataset
+# Line plots usually have time on the x-axis, showing how a single variable has changed over time
+# Path plots show how two variables have simultaneously changed over time, with time encoded in the way that observations are connected
+
+ggplot(economics, aes(date, unemploy / pop)) + geom_line() # This figure shows the unemployment rate
+ggplot(economics, aes(date, uempmed)) + geom_line() # This figure shows the median number of weeks unemployment
+
+# One major difference between the graphs is the last peak. Where the unemployment percentage is lower than it was in the preceding peaks, but the length of unemployment is high
+# This relationship could be examined in greater detail by plotting both time series on the same plot
+# However drawing a scatterplot of unemployment rate vs length of employment would prevent seeing the evolution over time. The solution for this is join points adjacent in time with line segments forming a path plot
+
+ggplot(economics, aes(unemploy / pop, uempmed)) + geom_path() + geom_point()
+
+# The plot above is hard to read because of the many line crossings, the direction in which time flows isn't as easy to see in the first plot. This can be fixed by coloring the points to see the direction of time
+
+year <- function(x)
+{
+  as.POSIXct(x)$year + 1900
+}
+
+ggplot(economics, aes(unemploy / pop, uempmed)) + geom_path(color = "grey60") + geom_point(aes(color = year(date)))
+
+# The graph shows that unemployment rate and length of unemployment are highly correlated, but in recent years the length of unemployment has been increasing relative to the unemployment rate
+
+###############################################################################################################################################################################################
+# 2.6.6. Exercises
+###############################################################################################################################################################################################
+# 1. 1. What’s the problem with the plot created by ggplot(mpg, aes(cty, hwy)) + geom point()? Which of the geoms described above is most effective at remedying the problem?
+
+ggplot(mpg, aes(cty,hwy)) + geom_point()
+
+# The problem of the graph is many of the points are overlapped on top of another. This issue could be solved by using geom_jitter()
+
+ggplot(mpg, aes(cty,hwy)) + geom_jitter()
+
+# 2. One challenge with ggplot(mpg, aes(class, hwy)) + geom boxplot() is that the ordering of class is alphabetical, which is not terribly useful. How could you change the factor levels to be more informative? Rather than reordering the factor by hand, you can do it automatically based on the data: ggplot(mpg, aes(reorder(class, hwy), hwy)) + geom boxplot(). What does reorder() do?
+
+ggplot(mpg, aes(class, hwy)) + geom_boxplot() # The graphs are all out of order and could be presented better
+
+# One way to change the ordering of the factors is by assigning a new set of levels to the factor to rearrange it into the correct order 
+
+mpg2 <- mpg
+mpg2$class <- factor(mpg$class, levels = c("midsize", "compact", "subcompact", "2seater", "minivan", "suv", "pickup"))
+ggplot(mpg2, aes(class, hwy)) + geom_boxplot()
+
+# Another way to reorder the graph is by using the reorder() function
+
+ggplot(mpg, aes(reorder(class, hwy), hwy)) + geom_boxplot()
+
+# Reorder() is a generic function. The first argument takes a categorical variable and reorder its based on the values of the second variable (usually numerically)
+
+# 3. Explore the distribution of the carat variable in the diamonds dataset. What binwidth reveals the most interesting patterns?
+
+ggplot(diamonds, aes(carat)) + geom_histogram() # The default binwidth shows that the vast majority of carats are less than 1 carat
+ggplot(diamonds, aes(carat)) + geom_histogram(binwidth = 0.1) # A binwidth of 0.1 shows that there a major spikes in certain carats values of diamonds with sharp decreases
+
+# 4. Explore the distribution of the price variable in the diamonds data. How does the distribution vary by cut?
+
+ggplot(diamonds, aes(price)) + geom_histogram() # Based on the plot, the vast majority of diamonds have a value equal or less than 5000 dollars, while a major majority of diamonds are worth less than 10000
+ggplot(diamonds, aes(price)) + geom_histogram(binwidth = 10) # Same trend of observed at above, however there appears to be a gap around where 1000 dollars should be indicating there are no diamonds worth that value
+ggplot(diamonds, aes(price)) + geom_histogram(binwidth = 100) # Same trends noticed as above
+ggplot(diamonds, aes(price)) + geom_histogram(binwidth = 1000) # This plot shows that almost a third of all diamonds are worth somewhere between 1000 and 2000 dollars
+ggplot(diamonds, aes(price)) + geom_histogram(binwidth = 5000) # Same conclusions as before, most diamonds are worth less than 10000 dollars
+
+ggplot(diamonds, aes(price, color = cut)) + geom_freqpoly() # Based on this plot the vast majority of diamonds of ideal cut make up the majority of diamonds priced under 5000
+ggplot(diamonds, aes(price, color = cut)) + geom_freqpoly(binwidth = 10) # It appears that higher cuts of diamonds are the only ones found at higher prices ranges after 10000
+ggplot(diamonds, aes(price, color = cut)) + geom_freqpoly(binwidth = 100) # The previous noted peak is visable once more for diamonds around 1000 dollars
+ggplot(diamonds, aes(price, color = cut)) + geom_freqpoly(binwidth = 1000) # Fair cut diamonds appear to be mostly only prevalent under the price range of 5000 
+ggplot(diamonds, aes(price, color = cut)) + geom_freqpoly(binwidth = 5000) # Same conclusions as before nothing new to add
+
+# 5. You now know (at least) three ways to compare the distributions of subgroups: geom violin(), geom freqpoly() and the colour aesthetic, or geom histogram() and facetting. What are the strengths and weaknesses of each approach? What other approaches could you try?
+# Histograms
+# Pros: Easy to find total counts of the different observations, showing the density of each bin. Great for smaller sets of data
+# Cons: Hard to find visualize the distribution compared to geom_freqpoly()
+
+# Frequency polygons
+# Pros: Easy to visualize the the distribution with a line compared to histograms. Works well with larger sets of data
+# Cons: None, it's perfect
+
+# Density estimates 
+# Pros: Takes a more mathematical approach to comparing the density of different groups
+# Cons: The math is too complicated and the assumptions the stat it uses are not true for all data, such as assuming the distribution is continuous, unbounded, and smooth
+
+ggplot(diamonds, aes(carat)) + geom_histogram()
+ggplot(diamonds, aes(price)) + geom_freqpoly()
+ggplot(diamonds, aes(price)) + geom_density()
+
+# 6. Read the documentation for geom bar().What does the weight aesthetic do?
+
+# In geom_bar() when the weight aesthetic is applied it makes the bar proportional to the sum of the weights for each of the cases in the group
+
+ggplot(mpg, aes(class)) + geom_bar(aes(weight = displ))
+
+# 7. Using the techniques already discussed in this chapter, come up with three ways to visualize a 2d categorical distribution. Try them out by visualizing the distribution of model and manufacturer, trans and class, and cyl and trans
+
+ggplot(mpg, aes(manufacturer, fill = model)) + geom_bar() # Each manufacturer appears to only create 1-3 models of each car
+ 
+ggplot(mpg, aes(trans, fill = class)) + geom_bar() # This graph shows the distribution of the different classes of car and which ones make up the majority of a specific transmission
+
+ggplot(mpg, aes(cyl, fill = trans)) + geom_bar() # This graph compares the size of the engine and the types of transmissions each of the cars are typically found in. We see a trend where most transmission fall more heavily into having smaller or heavier engines
+
+###############################################################################################################################################################################################
+# 2.7 Modifying the Axes
+###############################################################################################################################################################################################
+# There two families of useful helpers that allow for the modification of the x and y axis labels: xlab() and ylab()
+
+ggplot(mpg, aes(cty, hwy)) + geom_point(alpha = 1/3)
+
+ggplot(mpg, aes(cty, hwy)) + geom_point(alpha = 1/3) + xlab("city driving (mpg)") + ylab("highway driving (mpg)")
+
+# These labs can be removed by passing a NULL into the arguments of the function
+
+ggplot(mpg, aes(cty, hwy)) + geom_point(alpha = 1/3) + xlab(NULL) + ylab(NULL)
+
+# The functions xlim() and ylim() can be used to modify the limits of axes
+
+ggplot(mpg, aes(drv, hwy)) + geom_jitter(width = 0.25) 
+
+ggplot(mpg, aes(drv, hwy)) + geom_jitter(width = 0.25) + xlim("f", "r") + ylim(20,30) # Setting a limit on the axes will remove data that falls outside the limit, this goes for both x and y values
+
+# For a continuous scale, use NA to set only one limit. Also changing the axes limits sets values outside the range to NA. This can be avoided/suppressed with the associated warning na.rm = TRUE
+
+ggplot(mpg, aes(drv, hwy)) + geom_jitter(width = 0.25, na.rm = TRUE) + ylim(NA, 30)
+
+###############################################################################################################################################################################################
+# 2.8 Output
+###############################################################################################################################################################################################
+ # A majority of the time a plot object can be created and immediately plotted. But it's also possible to save a plot to a variable and manipulate it
+
+p <- ggplot(mpg, aes(displ, hwy, color = factor(cyl))) + geom_point()
+
+# Once a plot object is created there are several actions that can be done with it
+
+# 1. It can be rendered on screen with print(). This occurs automatically when running interactively, but within a loop or function it'll need to be print()
+
+print(p)
+
+# 2. It can be saved with ggsave()
+
+ggsave("test_save.png", width = 10, height = 10)
+
+# 3. Briefly describe it's structure with summary()
+
+summary(p)
+
+# 4. Save a cached copy of it with saveRDS(). This saves a complete copy of the plot object, so it can easily be recreated with readRDS()
+
+saveRDS(p, "plot.rds")
+q <- readRDS("plot.rds")
+
+# This form of storage is intended for short-term caching and not long-term storage due to the possibility of the plot changing over time
+
+###############################################################################################################################################################################################
+# 2.9. Quick Plots
+###############################################################################################################################################################################################
+# Quick plots can be created with a minimum amount of typing. Use the function qplot() over ggplot()
+# qplot() defines a plot in a single call, picking a geom by default if not supplied one. To use it, provide a set of aesthetics and a dataset
+# It still works but it is currently deprecated
+
+qplot(displ, hwy, data = mpg)
+qplot(displ, data = mpg)
+
+# qplot() attempts to pick a sensible geometry and statistic based on the arguments provided
+# Giving qplot() x and y variables will lead it to create a scatterplot. If provided just an x, it;ll create a histogram or bar chart depending on the variable
+# qplot() assumes that all variables should be scaled by default. To set an aesthetic to a constant, use I()
+
+qplot(displ, hwy, data = mpg, color = "blue")
+qplot(displ, hwy, data = mpg, color = I("blue"))
+
+# qplot() is useful for quick graphs but falls short for creating complex graphics with multiple aesthetics. In almost all cases ggplot should be used over qplot()
+
+###############################################################################################################################################################################################
+# End of Chapter 2
+###############################################################################################################################################################################################
