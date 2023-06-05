0639d748dadf0976c42bbbf0349b257f247c9e00
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-06-05 12:26:28 -0400

    Add files via upload

---
+++ Chapter_8_Themes.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+###############################################################################################################################################################################################
+# Chapter 8: Themes
+###############################################################################################################################################################################################
+# 8.1 Introduction
+###############################################################################################################################################################################################
+# The theme system allows for control over the non-data elements of the plot. The theme system does not affect how the data is rendered by geoms, or how it is transformed by scales
+# Themes don't change the perceptual properties of the plot, but they do help making the plot aesthetically pleasing or match an existing style guide
+# THemes give control over thigns like fonts, ticks, panel strips, and backgrounds
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
